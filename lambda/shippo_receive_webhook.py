f04333a705156dc1f78b050d91dcf7296be8e219
Patrick Bustos <saintpat97@gmail.com>
2021-11-02 11:43:10 -0700

    Changed to account for API gateway, basic dynamodb integration

--- lambda/shippo_receive_webhook.py
+++ lambda/shippo_receive_webhook.py
@@ -6,20 +6,21 @@ import os
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-### Simple Lambda webhook response. Typically returns 200 unless something in the backend is wrong.
+### Simple Lambda webhook response. Typically returns 200 unless something in the backend is wrong like a missing order_id or user_id.
 def lambda_handler(event, context):
-    logger.debug("[EVENT] event: {}".format(event))
-    logger.debug("[TRACKER] event metadata: {}".format(event['metadata']))
-    logger.debug("[TRACKER] event: {}".format(event['event']))
-    logger.debug("[TRACKER_DATA] data: {}".format(event['data']))
+    eventBody = event['body']
+    logger.debug("[EVENT] event: {}".format(eventBody))
+    logger.debug("[TRACKER] event metadata: {}".format(eventBody['metadata']))
+    logger.debug("[TRACKER] event: {}".format(eventBody['event']))
+    logger.debug("[TRACKER_DATA] data: {}".format(eventBody['data']))
 
-    print("[TRACKER_DATA] tracking_status: {}".format(event['data']['tracking_status']))
-    print("[TRACKER_DATA] tracking_number: {}".format(event['data']['tracking_number']))
-    print("[TRACKER_DATA] metadata: ".format(event['data']['metadata']))
+    print("[TRACKER_DATA] tracking_status: {}".format(eventBody['data']['tracking_status']))
+    print("[TRACKER_DATA] tracking_number: {}".format(eventBody['data']['tracking_number']))
+    print("[TRACKER_DATA] metadata: ".format(eventBody['data']['metadata']))
 
     order_id = None
-    if metaDataExists(event['data']['metadata']):
-        metadata = event['data']['metadata']
+    if metaDataExists(eventBody['data']['metadata']):
+        metadata = eventBody['data']['metadata']
         metadata = metadata.split(' ')
         order_id = metadata[1]
     else:
@@ -27,21 +28,7 @@ def lambda_handler(event, context):
         raise Exception("metadata does not exist in request")
     
     # Get the Shippo order data from DynamoDB using the data from the metadata field
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(os.environ['ORDERS_TABLE'])
-    response = table.get_item(
-        Key={
-            'order_id': order_id
-        }
-    )
-
-    if 'Item' not in response:
-        print(type(order_id))
-        print(order_id)
-        logger.error("[ERROR] Order data not found")
-        raise Exception("Order data not found")
-
-    order_data = response['Item']
+    order_data = getOrderResponse(order_id)
     logger.debug("[ORDER_DATA] order_data: {}".format(order_data))
     print("[ORDER_DATA] order_data: {}".format(order_data))
 
@@ -73,8 +60,42 @@ def getEmail(user_id):
     print("[USER_DATA] user_data: {}".format(user_data))
     return user_data['email']
 
+def getOrderResponse(order_id):
+    # Get the order data from DynamoDB using the order_id
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['ORDERS_TABLE'])
+    response = table.get_item(
+        Key={
+            'order_id': order_id
+        }
+    )
+    if "Item" not in response:
+        logger.error("[ERROR] Order data not found")
+        raise Exception("Order data not found")
+    order_data = response['Item']
+    logger.debug("[ORDER_DATA] order_data: {}".format(order_data))
+    print("[ORDER_DATA] order_data: {}".format(order_data))
+    return order_data
+
 def metaDataExists(metadata):
     if 'order_id' in metadata:
         return True
     else:
         return False
+
+def updateOrderStatus(order_id, status):
+    # Update the order status in DynamoDB
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['ORDERS_TABLE'])
+    response = table.update_item(
+        Key={
+            'order_id': order_id
+        },
+        UpdateExpression="set order_status = :s",
+        ExpressionAttributeValues={
+            ':s': status
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    logger.debug("[UPDATE_ORDER_STATUS] response: {}".format(response))
+    print("[UPDATE_ORDER_STATUS] response: {}".format(response))
