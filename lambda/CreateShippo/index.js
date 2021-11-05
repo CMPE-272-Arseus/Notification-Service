b000bfaf666c2a2fd5b6cb0a04f1de483561257a
Patrick Bustos <saintpat97@gmail.com>
2021-11-04 17:54:05 -0700

    Shippo shipment integration

--- lambda/CreateShippo/index.js
+++ lambda/CreateShippo/index.js
@@ -1,12 +1,58 @@
 const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB;
+const shippo = require("shippo")(process.env.SHIPPO_APIKEY);
+const { v4: uuid } = require('uuid');
 
 exports.handler = async (event) => {
-    var body = event.body;
-    var customerAddr = setCustomerAddress(body);
+    const body = event.body;
+    const customerAddr = setCustomerAddress(body.user);
+    const parcel = body.parcel;
+    console.log(customerAddr);
     
-    var storeAddr = await getStoreAddress("admin@gmail.com");
-    console.log(storeAddr);
+    const storeAddr = await getStoreAddress(body.store_id);
+
+    const shipment = await shippo.shipment.create({
+        "address_from": storeAddr,
+        "address_to": customerAddr,
+        "parcels": [parcel],
+        "async":false
+    }, function(err, shipment) {
+        if (err) {
+            console.log("[SHIPPO] create error: " + err);
+        } else {
+            console.log("[SHIPPO] create success: " + shipment);
+        }
+    });
+    console.log("[SHIPMENT] shipment: " + shipment);
+    const order_id = uuid();
+
+    //const rate = shipment.rates[0];
+    console.log(shipment.rates);
+    // const transaction = shippo.transaction.create({
+    //     "rate": rate.object_id,
+    //     "label_file_type": "PDF",
+    //     "metadata": {
+    //         "order_id": order_id
+    //     },
+    //     "async": false
+    // }, function(err, transaction) {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         console.log(transaction);
+    //     }
+    // });
+
+    // createCustomerOrder({
+    //     "order_id": order_id,
+    //     "user_id": body.user.user_id,
+    //     "status": "created",
+    //     "tracking_number": transaction.tracking_number,
+    //     "tracking_url": transaction.tracking_url,
+    //     "shippo_id": transaction.object_id,
+    //     "created_at": new Date().toISOString(),
+    //     "updated_at": new Date().toISOString()
+    // });
     
     const response = {
         statusCode: 200,
@@ -28,22 +74,32 @@ exports.handler = async (event) => {
 
 const getStoreAddress = async (storeId) => {
     console.log("[GET_STORE_ADDRESS] storeId: " + storeId);
-    var params = {
+    const params = {
       Key: {
-       "email": {
+       "store_id": {
          S: storeId
         }, 
       }, 
       TableName: process.env.STORE_TABLE
      };
-    var data =  await dynamo.getItem(params).promise();
-    console.log("[GET_STORE_ADDRESS] dynamo response: " + data);
-    return data;
+    const data =  await dynamo.getItem(params).promise();
+    console.log("[GET_STORE_ADDRESS] dynamo response: " + JSON.stringify(data));
+    return {
+        "city": data.Item.city.S,
+        "zip": data.Item.zip.S,
+        "state": data.Item.state.S,
+        "country": data.Item.country.S,
+        "street1": data.Item.street1.S,
+        "store_name": data.Item.store_name.S,
+        "phone": data.Item.phone.S,
+        "email": data.Item.email.S,
+        "store_id": data.Item.store_id.S
+    };
 };
 
 const setCustomerAddress = (data) => {
     console.log("[SET_CUSTOMER_ADDRESS] data: " + data);
-    var name = data.first_name + " " + data.last_name;
+    const name = data.first_name + " " + data.last_name;
     return {
         'name': name,
         'street1': data.street1,
@@ -55,3 +111,43 @@ const setCustomerAddress = (data) => {
         'email': data.email
     };
 }
+
+const createCustomerOrder = async (data) => {
+    console.log("[CREATE_CUSTOMER_ORDER] data: " + data);
+    dynamo.putItem({
+        TableName: process.env.ORDER_TABLE,
+        Item: {
+            "order_id": {
+                S: data.order_id
+            },
+            "user_id": {
+                S: data.user_id
+            },
+            "order_status": {
+                S: "PRE_TRANSIT"
+            },
+            "tracking_number": {
+                S: data.tracking_number
+            },
+            "tracking_url": {
+                S: data.tracking_url
+            },
+            "shippo_id": {
+                S: data.shippo_id
+            },
+            "created_at": {
+                S: data.created_at
+            },
+            "updated_at": {
+                S: data.updated_at
+            }
+        }
+    }, (err, data) => {
+        if (err) {
+            console.log("[CREATE_CUSTOMER_ORDER] error: " + err);
+        } else {
+            console.log("[CREATE_CUSTOMER_ORDER] success: " + data);
+        }
+    }
+    );
+};
