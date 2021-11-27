1bf68d08d3e312d2c6d6cfd91c801a75a1004df4
Patrick Bustos <saintpat97@gmail.com>
2021-11-27 15:48:37 -0800

    Did not return data -.-

--- lambda/CreateShippo/index.js
+++ lambda/CreateShippo/index.js
@@ -189,6 +189,7 @@ const updateCustomerOrder = async (data) => {
             console.log("[CREATE_CUSTOMER_ORDER] error: " + err);
         } else {
             console.log("[CREATE_CUSTOMER_ORDER] success: " + data);
+            return data;
         }
     }
     ).promise();
