24b32b3b27b6969d9090f72c209e5ad1eabf581e
Patrick Bustos <saintpat97@gmail.com>
2021-11-28 17:47:26 -0800

    Removed await from dynamo

--- lambda/CreateShippo/index.js
+++ lambda/CreateShippo/index.js
@@ -148,7 +148,7 @@ const getStoreAddress = async (storeId) => {
       }, 
       TableName: process.env.STORE_TABLE
      };
-    const data =  await dynamo.getItem(params);
+    const data =  dynamo.getItem(params);
     console.log("[GET_STORE_ADDRESS] dynamo response: " + data);
     return {
         "city": data.Item.city.S,
